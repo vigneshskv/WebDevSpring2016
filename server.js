e108100772a5d21db34df875049258675e142e0b
vigneshs <vigneshs@ccs.neu.edu>
2016-02-01 19:45:48 -0500

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+// Given any URL, first try to match for routes, if none match then go into /public directory and match physical file
+// no pattern match inside /public directory, only match physical files
+// index.html is a special case, so even if no file specifically mentioned, it gives index.html = default
+
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
