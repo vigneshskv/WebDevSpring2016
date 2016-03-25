3bdaa2dfd53e0a33217b1ef63fe00ffe46acc7c4
vigneshs <vigneshs@ccs.neu.edu>
2016-03-25 17:47:19 -0400

    Added  /public/assignment/server/services/form.service.js

---
+++ public/assignment/server/services/form.service.js
@@ -0,0 +1,32 @@
+module.exports = function (app, formModel){
+    app.get("/api/assignment/user/:userId/form", findFormByUserId);
+    app.get("/api/assignment/form/:formId", findFormById);
+    app.delete("/api/assignment/form/:formId", deleteFormById);
+    app.post("/api/assignment/user/:userId/form", createForm);
+    app.put("/api/assignment/form/:formId", updateFormById);
+
+    function findFormByUserId(request,result){
+        var forms = formModel.findFormByUserId(request.params.userId);
+        result.json(forms);
+    }
+
+    function findFormById(request,result){
+        var form = formModel.findFormById(request.params.formId);
+        result.json(form)
+    }
+
+    function deleteFormById(request,result){
+        var forms = formModel.deleteFormById(request.params.formId);
+        result.json(forms);
+    }
+
+    function createForm(request,result){
+        var newForms = formModel.createForm(req.body, request.params.userId);
+        result.json(newForms);
+    }
+
+    function updateFormById(request,result){
+        var forms = formModel.updateFormById(request.body, request.params.formId);
+        result.json(forms);
+    }
+};
