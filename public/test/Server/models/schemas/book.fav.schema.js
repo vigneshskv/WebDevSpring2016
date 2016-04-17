829cdcde7bcf8638caed58f287f83ddb1e128048
vigneshs <vigneshs@ccs.neu.edu>
2016-04-16 23:40:32 -0400

    added project schemas

---
+++ public/test/Server/models/schemas/book.fav.schema.js
@@ -0,0 +1,12 @@
+"use strict";
+module.exports = function(mongoose) {
+    var breBookFavSchema =  mongoose.Schema({
+            userId          : String,
+            //each bookId is the 13  or 10 digit ISBN
+            bookIds          : [String]
+            //bookIds          : {type: [String], unique : true}
+        },
+        {collection: "bukreview.test4.bookfav"});
+
+    return breBookFavSchema;
+};
